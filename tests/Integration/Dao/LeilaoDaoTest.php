a1a2184ad1d9b6910c4ffe57aab32b0f2f03da3b
wellysonlee <martinsgwellyson@gmail.com>
2024-02-28 11:04:41 -0300

    testando o banco de dados

---
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\Leilao\Tests\Integration\Dao;
+
+use Alura\Leilao\Dao\Leilao as DaoLeilao;
+use Alura\Leilao\Infra\ConnectionCreator;
+use Alura\Leilao\Model\Leilao;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoDao extends TestCase
+{
+    public function testInsercaoEBuscaDevemFuncionar () 
+    {
+        $leilao = new Leilao('Variante 0Km');
+        $leilaoDao = new DaoLeilao (ConnectionCreator::getConnection());
+
+        $leilaoDao->salva($leilao);
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        self::assertCount(1,$leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame('Variante 0Km',$leiloes[0]->recuperarDescricao());
+
+    }
+}

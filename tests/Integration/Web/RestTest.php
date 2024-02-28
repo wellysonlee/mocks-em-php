07aff8d38b5675e073548fa49e538623e23c1dc2
wellysonlee <martinsgwellyson@gmail.com>
2024-02-28 16:55:23 -0300

    testando APIs

---
+++ tests/Integration/Web/RestTest.php
@@ -0,0 +1,15 @@
+<?php 
+namespace Alura\Leilao\Tests\Integration\Web;
+
+use PHPUnit\Framework\TestCase;
+
+class RestTest extends TestCase
+{
+    public function testApiTestDeveRetornarArraydeLeiloes()
+    {
+        $resposta = file_get_contents('http://localhost:8080/rest.php');
+
+        self::assertStringContainsString('200 OK', $http_response_header[0]);
+        self::assertIsArray(json_decode($resposta));
+    }
+}

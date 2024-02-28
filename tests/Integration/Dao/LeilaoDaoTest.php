08423805a80eb45973838074fe854301f3444dec
wellysonlee <martinsgwellyson@gmail.com>
2024-02-28 12:38:09 -0300

    garantindo a integridade

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -9,17 +9,41 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoDao extends TestCase
 {
+    
+    private static \PDO $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$pdo = new \PDO('sqlite::memory:');
+        self::$pdo->exec('create table leiloes(
+            id INTEGER primary key,
+            descricao TEXT,
+            finalizado BOOL, 
+            dataInicio TEXT);'); 
+    }
+    protected function setUp():void
+    {
+        self::$pdo->beginTransaction();
+    }
     public function testInsercaoEBuscaDevemFuncionar () 
     {
+        //arraneg
         $leilao = new Leilao('Variante 0Km');
-        $leilaoDao = new DaoLeilao (ConnectionCreator::getConnection());
-
+        $leilaoDao = new DaoLeilao (self::$pdo);
         $leilaoDao->salva($leilao);
+
+        //act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        //assert
         self::assertCount(1,$leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame('Variante 0Km',$leiloes[0]->recuperarDescricao());
+    }
+
+    protected function tearDown(): void
+    {
+        self::$pdo->rollBack();
 
     }
 }
